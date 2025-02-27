c9599d1178d6260c0722dd77479de9c690e37b3e
rampathak <ram.pathak65@gmail.com>
2025-02-27 11:41:43 -0500

    Update Reversenumber.java

--- Reversenumber.java
+++ Reversenumber.java
@@ -6,7 +6,19 @@ public class Reversenumber {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number");
+		int num=sc.nextInt();
+		int rev=0;
+		while(num!=0)
+		{
+			rev=rev*10+num%10;
+			num=num/10;
+			
+		}
+		System.out.println("Reverse number is :" +rev );
+		
+		
 		
 
 }
